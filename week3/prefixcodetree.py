e00e017f0aa9e29f60d316f7f43bbf70d1df2663
duong755 <ngo.quang.duong.1100@gmail.com>
2020-10-01 12:18:13 +0700

    Implement decoded method

--- week3/prefixcodetree.py
+++ week3/prefixcodetree.py
@@ -21,5 +21,26 @@ class PrefixCodeTree:
                 current_node = current_node.get("1")
         current_node.__setitem__("leaf", symbol)
 
-    def decode(self, encodedData: str, datalen: int) -> str:
-        pass
+    @staticmethod
+    def _hex_to_binary(bytes_literal: bytes) -> str:
+        hex_string: str = bytes_literal.hex()
+        decimal: int = int(hex_string, base=16)
+        binary_string = bin(decimal)[2:]
+        return binary_string
+
+    def decode(self, encodedData: bytes, datalen: int) -> List[str]:
+        binary_string = PrefixCodeTree._hex_to_binary(encodedData)
+        tree = self.json()
+        current_node = tree
+
+        symbols: List[str] = []
+        loop_len = min(datalen, binary_string.__len__())
+
+        for i in range(loop_len):
+            bit: str = binary_string[i]
+            current_node = current_node.get(bit)
+            if "leaf" in current_node:
+                symbols.append(current_node.get("leaf"))
+                current_node = tree
+
+        return symbols
--- week3/test.py
+++ week3/test.py
@@ -10,6 +10,8 @@ codebook: dict = {
 
 codeTree = PrefixCodeTree()
 
-codeTree.insert([1, 0, 1], "a")
-codeTree.insert([1, 1, 1], "b")
+for symbol in codebook:
+    codeTree.insert(codebook[symbol], symbol)
+
 print(json.dumps(codeTree.json(), indent=2, sort_keys=True))
+print(codeTree.decode(b"\xd2\x9f\x20", 21))
